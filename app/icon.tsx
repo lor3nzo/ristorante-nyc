b2032e1ce5743f7fe58f5d8f1d5c7eeba7574038
lor3nzo <me@lor3nzo.com>
2026-02-27 23:46:52 -0500

    add Italian flag favicon via CSS svg

---
+++ app/icon.tsx
@@ -0,0 +1,12 @@
+export const size = { width: 32, height: 32 }
+export const contentType = 'image/png'
+
+export default function Icon() {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0" y="0" width="11" height="32" fill="#009246"/>
+      <rect x="11" y="0" width="10" height="32" fill="#ffffff"/>
+      <rect x="21" y="0" width="11" height="32" fill="#ce2b37"/>
+    </svg>
+  )
+}
