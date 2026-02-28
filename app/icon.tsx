9997eeec2510678bdfb74aa1297e05b83efbf1ce
lor3nzo <me@lor3nzo.com>
2026-02-27 23:50:34 -0500

    fix icon route

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,12 +1,17 @@
+import { ImageResponse } from 'next/og'
+
 export const size = { width: 32, height: 32 }
 export const contentType = 'image/png'
 
 export default function Icon() {
-  return (
-    <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-      <rect x="0" y="0" width="11" height="32" fill="#009246"/>
-      <rect x="11" y="0" width="10" height="32" fill="#ffffff"/>
-      <rect x="21" y="0" width="11" height="32" fill="#ce2b37"/>
-    </svg>
+  return new ImageResponse(
+    (
+      <div style={{ display: 'flex', width: 32, height: 32 }}>
+        <div style={{ width: 11, height: 32, background: '#009246' }} />
+        <div style={{ width: 10, height: 32, background: '#ffffff' }} />
+        <div style={{ width: 11, height: 32, background: '#ce2b37' }} />
+      </div>
+    ),
+    { ...size }
   )
 }
